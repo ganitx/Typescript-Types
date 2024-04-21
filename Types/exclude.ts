af61f05d6775db189c4ea18e867cc7993b3e426b
Gani <agani2032@gmail.com>
2024-04-21 11:41:41 +0300

    challenge: 5

---
+++ Types/exclude.ts
@@ -0,0 +1,9 @@
+/*
+  Implement the built-in `Exclude<T, U>`
+
+  > Exclude from `T` those types that are assignable to `U`
+
+  type Result = MyExclude<'a' | 'b' | 'c', 'a'> // 'b' | 'c'
+*/
+
+type MyExclude<T, U> = T extends U ? never : T;  
