81f0681dd265469531a9702e5472518005f0cd29
Gani <agani2032@gmail.com>
2024-04-26 09:53:20 +0300

    TupleToUnion

---
+++ Types/tupleTouni.ts
@@ -0,0 +1,11 @@
+/*
+    Implement a generic `TupleToUnion<T>` which covers the values of a tuple to its values union.
+
+    type Arr = ['1', '2', '3']
+
+    type Test = TupleToUnion<Arr> // expected to be '1' | '2' | '3'
+*/
+
+type TupleToUnion<T> =  {
+    [key in keyof T]: T[key];
+};
